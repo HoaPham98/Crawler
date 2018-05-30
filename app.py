b0dbff0470d55c80b59e5315afac8cc15bc86f38
HoaPQ <rubik0403@gmail.com>
2018-05-30 23:11:31 +0700

    Fix

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from proxybroker import Broker
 import os
 
-broker = Broker()
-host, port = '127.0.0.1', 8080
-types = [('HTTP', 'High')]
-broker.serve(host=host, port=port, types=types, limit=10, max_tries=3,prefer_connect=True, min_req_proxy=5, max_error_rate=0.5, max_resp_time=8, backlog=100, countries=['VN'])
+def main():
+    broker = Broker()
+    host, port = '127.0.0.1', 8080
+    types = [('HTTP', 'High')]
+    broker.serve(host=host, port=port, types=types, limit=10, max_tries=3,prefer_connect=True, min_req_proxy=5, max_error_rate=0.5, max_resp_time=8, backlog=100, countries=['VN'])
+
+if __name__ == '__main__':
+    main()
